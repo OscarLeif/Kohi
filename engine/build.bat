a31975e467b1b7215225fd8596974d058b350404
Oscar Fernandez <oscar.leif@gmail.com>
2024-03-01 17:36:59 -0500

    Fixed issues with setup
    Catching typos in scripting languages.

--- engine/build.bat
+++ engine/build.bat
@@ -1,21 +1,21 @@
-REM Build script for the engine
+REM Build script for engine
 @ECHO OFF
-setLocal EnableDelayedExpansion
+SetLocal EnableDelayedExpansion
 
-REM Get a list of all the .c files
+REM Get a list of all the .c files.
 SET cFilenames=
 FOR /R %%f in (*.c) do (
-    SET cFilenames =!cFilenames! %%f
+    SET cFilenames=!cFilenames! %%f
 )
 
-REM echo "Files: " %cFilenames%
+REM echo "Files:" %cFilenames%
 
 SET assembly=engine
 SET compilerFlags=-g -shared -Wvarargs -Wall -Werror
 REM -Wall -Werror
 SET includeFlags=-Isrc -I%VULKAN_SDK%/Include
-SET linkerFlags=-Iuser32 -lvulkan-1 -L%VULKAN_SDK%/Lib
-SET defines =-D_DEBUG -DKEXPORT -D_CRT_SECURE_NOWARNINGS
+SET linkerFlags=-luser32 -lvulkan-1 -L%VULKAN_SDK%/Lib
+SET defines=-D_DEBUG -DKEXPORT -D_CRT_SECURE_NO_WARNINGS
 
 ECHO "Building %assembly%%..."
 clang %cFilenames% %compilerFlags% -o ../bin/%assembly%.dll %defines% %includeFlags% %linkerFlags%
