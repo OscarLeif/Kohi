078e8671cc014c89f1d3ca75c606029dfa79ec2a
Oscar Fernandez <oscar.leif@gmail.com>
2024-03-06 00:39:41 -0500

    completed Kohi #0003 Logging and Assertions
    one of the issues.
    #include "asserts.h" != #include "assert.h"
    Visual studio have a header file like that.

--- engine/src/core/logger.c
+++ engine/src/core/logger.c
@@ -1,29 +1,30 @@
 #include "logger.h"
+#include "asserts.h"
 
 // TODO: temporary
 #include <stdio.h>
 #include <string.h>
 #include <stdarg.h>
 
-b8 initialize_loggin(){
+b8 initialize_loggin() {
     // TODO: create log file
     return TRUE;
 }
-void shutdown_loggin(){
+void shutdown_loggin() {
     // TODO: cleanup loggin/write queued entries
 }
 
-KAPI void log_output(log_level level, const char* message, ...){
-    const char* level_string[6] = {"[FATAL]: ","[ERROR]: ","[WARN]: ","[INFO]: ","[DEBUG]: ","[TRACE]: "};
+void log_output(log_level level, const char* message, ...) {
+    const char* level_string[6] = {"[FATAL]: ", "[ERROR]: ", "[WARN]: ", "[INFO]: ", "[DEBUG]: ", "[TRACE]: "};
     // b8 is_error = level<2;
 
     // Technically imposes a 32k charatcer limit on a single log entry, but...
-    //DON'T DO THAT!
+    // DON'T DO THAT!
     char out_message[32000];
-    memset(out_message,0, sizeof(out_message));
+    memset(out_message, 0, sizeof(out_message));
 
     __builtin_va_list arg_ptr;
-    va_start(arg_ptr,message);
+    va_start(arg_ptr, message);
     vsnprintf(out_message, 32000, message, arg_ptr);
     va_end(arg_ptr);
 
@@ -32,4 +33,8 @@ KAPI void log_output(log_level level, const char* message, ...){
 
     // TODO: platform-specific output.
     printf("%s", out_message2);
+}
+
+void report_assertion_failure(const char* expression, const char* message, const char* file, i32 line) {
+    log_output(LOG_LEVEL_FATAL, "Assertion Failure: %s, message: '%s', in file %s, line: %d\n", expression, message, file, line);
 }
